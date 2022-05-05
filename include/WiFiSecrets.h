11ce2cad9f300156af025cfbed08eb439c14ac50
Olaf <github@dacht.net>
2022-05-05 17:09:14 +0200

    Added WEB UsER and PASS to the WIFISecrets.h template

--- include/WiFiSecrets.h
+++ include/WiFiSecrets.h
@@ -4,6 +4,9 @@
 #define WIFI_SSID "MYNET"
 #define WIFI_PASS "WifiPas$$word"
 
+#define WEB_USER "admin"
+#define WEB_PASS "silvia"
+
 #define MQTT_HOST "mqtt.example.com"
 #define MQTT_PORT 1883
 #define MQTT_USER "MQTTUser"
