b3aa98b42719e6f3fcc5d4da317c5285bef81e82
Olaf <github@dacht.net>
2023-11-16 11:06:17 +0100

    TuningThreshold failsafe fix

--- src/webInterfaceAPI.cpp
+++ src/webInterfaceAPI.cpp
@@ -274,12 +274,13 @@ void webInterfaceAPI::handleSet(AsyncWebServerRequest *request) {
     } else if (request->argName(i) == "tunethres") {
       addjson(message, firstarg, "tunethres", request->arg(i));
       myMachine->myTuner->setTuneThres((request->arg(i)).toDouble());
+            if ((request->arg(i)).toDouble() > 100) { // failsafe
+        myMachine->myTuner->setTuneThres(100);
+      }
     } else if (request->argName(i) == "tunestep") {
       addjson(message, firstarg, "tunestep", request->arg(i));
       myMachine->myTuner->setTuneStep((request->arg(i)).toDouble());
-      if ((request->arg(i)).toDouble() > 110) { // failsafe
-        myMachine->myTuner->setTuneThres(110);
-      }
+
     } else if (request->argName(i) == "tuningOn") {
       if (String("true").equalsIgnoreCase(request->arg(i))) {
         myMachine->myTuner->tuning_on();
