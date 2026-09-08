1fca0b87d6e0196929086b6109ad1e11eeed2946
Olaf <github@dacht.net>
2026-09-08 16:08:30 +0200

    WIFISecrets.h Template

---
+++ include/WiFiSecrets.h
@@ -0,0 +1,16 @@
+#ifndef WIFI_SECRETS_H
+#define WIFI_SECRETS_H
+
+#define WIFI_SSID "YourSSID"
+#define WIFI_PASS "PASSWORD"
+
+#define WEB_USER "admin"
+#define WEB_PASS "your password"
+
+#define MQTT_HOST "192.168.178.205"
+#define MQTT_PORT 1883
+#define MQTT_USER "mqttUser"
+#define MQTT_PASS "mqttPass"
+#define MQTT_TOPIC "Espresso"
+
+#endif
